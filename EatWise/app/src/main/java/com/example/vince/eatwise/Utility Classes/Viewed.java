f85dd9067295a2b1d85e95c0a19c897e27374bab
MaximusZCL <32944477+MaximusZCL@users.noreply.github.com>
2017-11-14 22:05:10 -0800

    Add files via upload

---
+++ EatWise/app/src/main/java/com/example/vince/eatwise/Utility Classes/Viewed.java
@@ -0,0 +1,13 @@
+package com.example.vince.eatwise;
+
+import java.util.Date;
+
+/**
+ * Created on 11/14/17.
+ */
+
+public class Viewed {
+    public Date date;
+    public Restaurant restaurant;
+    public Boolean visit;
+}
