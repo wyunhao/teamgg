3751d6d4542377cbe855a451091cfd99a8052e7c
vincent <vincentzj@ucla.edu>
2017-11-16 08:10:51 +0800

    SearchResultActivity implemented

--- EatWise/app/src/main/java/com/example/vince/eatwise/SearchResultActivity.java
+++ EatWise/app/src/main/java/com/example/vince/eatwise/SearchResultActivity.java
@@ -1,39 +1,48 @@
 package com.example.vince.eatwise;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.vince.eatwise.QueryData.QueryFilter;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.yelp.clientlib.connection.YelpAPI;
 import com.yelp.clientlib.connection.YelpAPIFactory;
 import com.yelp.clientlib.entities.Business;
 import com.yelp.clientlib.entities.SearchResponse;
 import com.yelp.clientlib.entities.options.CoordinateOptions;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Response;
 
 public class SearchResultActivity extends AppCompatActivity {
-    String client_id = "2S9ar0NXnZr9RFPo25zgBA";
-    String client_secret = "Llc2Gjdua4U4llGf532aspiuQs841gmbGhvGQBOhCJhhoxKscYkV79H7UFKvMElB";
-    String token = "Bearer";
-    String token_secret = "KVIzqS5JwpvzDhTOELrii4Yl563Yg2FTxaaJBmNKIi0Igkhd3dCDhWfslEq3jLkG1ZQ7_aX6MZUgp2oWtU32GkdLsVGvpGmRJstpQFcAQpvnME8Kqx-ZufrRuZoLWnYx";
-
-//    YelpAPIFactory apiFactory = null;
-//    YelpAPI yelpAPI = null;
+    private final String client_id = "2S9ar0NXnZr9RFPo25zgBA";
+    private final String client_secret = "Llc2Gjdua4U4llGf532aspiuQs841gmbGhvGQBOhCJhhoxKscYkV79H7UFKvMElB";
+    private final String token = "Bearer";
+    private final String token_secret = "KVIzqS5JwpvzDhTOELrii4Yl563Yg2FTxaaJBmNKIi0Igkhd3dCDhWfslEq3jLkG1ZQ7_aX6MZUgp2oWtU32GkdLsVGvpGmRJstpQFcAQpvnME8Kqx-ZufrRuZoLWnYx";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        TextView textView = (TextView) findViewById(R.id.textView_id);
 
         final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,51 +51,57 @@ public class SearchResultActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-//        createAPI();
-//        Intent intent = getIntent();
-//        QueryFilter filter = (QueryFilter) intent.getExtras().getSerializable("query"); //gets the query filter
-
+        Intent intent = getIntent();
+        QueryFilter filter = (QueryFilter) intent.getExtras().getSerializable("filter"); //gets the query filter
+        String url = generateURL(filter);
+        new APIcall().execute(url);
     }
 
-//    private void createAPI(){
-//        apiFactory = new YelpAPIFactory(client_id, client_secret, token, token_secret);
-//        yelpAPI = apiFactory.createAPI();
-//    }
-//
-//    private void APIsearch(QueryFilter filter){
-//        try {
-//            String ll = filter.getLocation();
-//            String latitude = "";
-//            String longitude = "";
-//            for (int i = 0; i < ll.length(); i++) {
-//                char c = ll.charAt(i);
-//                if (c == ',') {
-//                    latitude = ll.substring(0, i);
-//                    longitude = ll.substring(i + 1);
-//                }
-//            }
-//            Double latitude_d = Double.parseDouble(latitude);
-//            Double longitude_d = Double.parseDouble(longitude);
-//            Long radius = filter.getDistance();
-//            String radius_s = radius + "";
-//            String keyword = filter.getKeyword();
-//            Map<String, String> params = new HashMap<>();
-//            params.put("term", keyword);
-//            params.put("radius_filter", radius_s);
-//            CoordinateOptions coordinate = CoordinateOptions.builder()
-//                    .latitude(latitude_d)
-//                    .longitude(longitude_d).build();
-//            Call<SearchResponse> call = yelpAPI.search(coordinate, params);
-//            Response<SearchResponse> response = call.execute();
-//            SearchResponse searchResponse = response.body();
-//
-//            ArrayList<Business> businesses = searchResponse.businesses();
-//            String businessName = businesses.get(0).name();
-//            Double rating = businesses.get(0).rating();
-//        }
-//        catch(IOException e){
-//            e.printStackTrace();
-//        }
-//    }
+    private String generateURL(QueryFilter filter){
+        String keyword = "food";
+        Integer distance = filter.getDistance();
+        String distance_s = distance + "";
+        String location = filter.getLocation();
+        String queryURL = "https://api.yelp.com/v3/businesses/search?";
+        queryURL += "term=" + keyword + "&";
+        queryURL += "location=" + location + "&";
+        queryURL += "radius=" + distance_s + "&";
+        queryURL += "limit=10";
+        return queryURL;
+    }
+
+    private class APIcall extends AsyncTask<String, Void, String>{
+
+        protected String doInBackground(String... params) {
+            try {
+                String queryURL = params[0];
+                URL url = new URL (queryURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty  ("Authorization", "Bearer " + "KVIzqS5JwpvzDhTOELrii4Yl563Yg2FTxaaJBmNKIi0Igkhd3dCDhWfslEq3jLkG1ZQ7_aX6MZUgp2oWtU32GkdLsVGvpGmRJstpQFcAQpvnME8Kqx-ZufrRuZoLWnYx");
+                InputStream content = (InputStream)connection.getInputStream();
+                BufferedReader in   =
+                        new BufferedReader (new InputStreamReader(content));
+                String line;
+                String JsonStr = "";
+                while ((line = in.readLine()) != null) {
+                    JsonStr += line;
+                }
+                Gson gson = new Gson();
+                JsonObject jsonObject = gson.fromJson(JsonStr, JsonObject.class);
+                JsonArray businesses_arr =  jsonObject.getAsJsonArray("businesses");
+                String name = businesses_arr.get(0).getAsJsonObject().get("name").getAsString();
+                return name;
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        protected void onPostExecute(String result) {
+            TextView txt = (TextView) findViewById(R.id.textView_id);
+            txt.setText(result);
+        }
+    }
 }
