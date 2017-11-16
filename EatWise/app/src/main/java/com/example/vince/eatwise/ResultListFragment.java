9e2209229fabd4578c74377d181f2b19e597b1b1
shirleychen920 <chenhaojie94@gmail.com>
2017-11-16 01:33:23 -0800

    create list view to display results

--- EatWise/app/src/main/java/com/example/vince/eatwise/ResultListFragment.java
+++ EatWise/app/src/main/java/com/example/vince/eatwise/ResultListFragment.java
@@ -1,5 +1,6 @@
 package com.example.vince.eatwise;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
@@ -7,12 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
+
+import java.util.ArrayList;
 
 import static android.view.Gravity.CENTER;
 
@@ -37,28 +43,14 @@ public class ResultListFragment extends Fragment {
     }
 
     private void populateResults() {
-
+        String[] itemName = new String[results.size()];
         for (int i = 0; i < results.size(); i++) {
-            RelativeLayout relativeLayout = new RelativeLayout(getContext());
-            TextView textView = new TextView(getContext());
-
-            String name = results.get(i).getAsJsonObject().get("name").getAsString();
-            textView.setText(name);
-
-//            RelativeLayout.LayoutParams lpView = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, 100);
-//            lpView.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-//            lpView.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-//
-//            relativeLayout.setLayoutParams(lpView);
-//            relativeLayout.addView(textView);
-//            relativeLayout.setGravity(CENTER);
-
-
-
-
-            LinearLayout resultForm = myView.findViewById(R.id.result_form);
-            resultForm.addView(relativeLayout);
+            itemName[i]= results.get(i).getAsJsonObject().get("name").getAsString();
         }
+        Integer imageID[] = new Integer[20];
+        CustomListAdapter adapter=new CustomListAdapter(getActivity(), itemName, imageID);
+        ListView list=(ListView)myView.findViewById(R.id.list);
+        list.setAdapter(adapter);
 
         return;
 
@@ -66,4 +58,33 @@ public class ResultListFragment extends Fragment {
 
 
 
+}
+
+class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity contextActivity;
+    private final String[] itemName;
+
+
+    public CustomListAdapter(Activity contextActivity, String[] itemName, Integer[] imageID) {
+        super(contextActivity, R.layout.list_row, itemName);
+        // TODO Auto-generated constructor stub
+
+        this.contextActivity=contextActivity;
+        this.itemName = itemName;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=contextActivity.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_row, null,true);
+
+        TextView itemTitle = (TextView) rowView.findViewById(R.id.item);
+//        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+//        TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+
+        itemTitle.setText(itemName[position]);
+
+        return rowView;
+
+    };
 }
