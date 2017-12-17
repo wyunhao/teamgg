29f760610a15c668f9a2c020d08d8a69ce15887f
MaximusZCL <32944477+MaximusZCL@users.noreply.github.com>
2017-12-16 23:44:28 -0800

    added constants for refactored history
    to improve readability and support easier change to code

--- EatWise/app/src/main/java/com/example/vince/eatwise/Constants/Constant.java
+++ EatWise/app/src/main/java/com/example/vince/eatwise/Constants/Constant.java
@@ -1,9 +1,21 @@
 package com.example.vince.eatwise.Constants;
 
+import java.util.function.DoubleUnaryOperator;
+import java.util.zip.DeflaterOutputStream;
+
 public final class Constant {
     public static final Integer HISTORY_LENGTH = 100;
     public static final Integer NUM_RECOMMEND = 10;
 
     public static final Double VIEW_WEIGHT = 1.0;
     public static final Double VISIT_WEIGHT = 20.0;
+
+    public static final Double INIT_PREF_COST = 15.0;
+    public static final Double INIT_PREF_RATING = 4.0;
+    public static final Double INIT_PREF_DISTANCE = 1000.0;
+    public static final String INIT_PREF_TYPE = "food";
+
+    public static final Double DISTANCE_WEIGHT = 4.0;
+    public static final Double RATING_WEIGHT = 3.0;
+    public static final Double COST_WEIGHT = 4.0;
 }
